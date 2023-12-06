1f950229ed344172cf53b4afbee5c4b60430c9e9
gustav <gustav.lindqvist000@gmail.com>
2023-12-06 11:05:00 +0100

    day6 done

--- day6/src/star2.rs
+++ day6/src/star2.rs
@@ -2,6 +2,6 @@ pub fn star2(){
     let (l1, l2) = include_str!("data.in").split_once("\n").unwrap();
     let t = l1.split_whitespace().skip(1).collect::<Vec<_>>().join("").parse::<u64>().unwrap();
     let d = l2.split_whitespace().skip(1).collect::<Vec<_>>().join("").parse::<u64>().unwrap();
-    let l = (1..(t)).map(move |h| (h*(t-h) > d) as u32).sum::<u32>();
-    dbg!(l);
+    let solutions = (1..(t)).map(move |h| (h*(t-h) > d) as u32).sum::<u32>();
+    print!("{:?}\n", solutions);
 }
