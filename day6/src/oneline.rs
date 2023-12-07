8c68e60598323ab3b6eee24e26baa88e5dcb95f5
gustav <gustav.lindqvist000@gmail.com>
2023-12-07 11:11:56 +0100

    day7 done

--- day6/src/oneline.rs
+++ day6/src/oneline.rs
@@ -1,1 +1,11 @@
-pub fn oneline(){}
+// pub fn oneline(){
+//     [false, true].iter().map(|b| include_str!("data.in").split("\n").map(|l| l.split_whitespace().skip(1).map(|s| s.parse::<f64>().unwrap()).collect::<Vec<_>>()));
+// }
+
+pub fn oneline(){
+    let (l1, l2) = include_str!("data.in").split_once("\n").unwrap();
+    let t = l1.split_whitespace().skip(1).collect::<Vec<_>>().join("").parse::<f64>().unwrap();
+    let d = l2.split_whitespace().skip(1).collect::<Vec<_>>().join("").parse::<f64>().unwrap();
+    let solutions = f64::sqrt(f64::powi(t/2.0,2) - d).floor() * 2.0 + 1.0;
+    print!("{:?}\n", solutions as u64);
+}
---
+++ day7/src/star1.rs
@@ -0,0 +1,19 @@
+use std::collections::HashMap;
+
+pub fn star1(){
+    let order = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'].iter().enumerate().map(|t| (t.1, t.0)).collect::<HashMap<_,_>>();
+    let mut pairs = include_str!("data.in").lines().map(|l| l.split_once(" ").unwrap()).collect::<Vec<_>>();
+    for i in (0..=4).rev(){
+        pairs.sort_by(|a,b| order.get(&b.0.chars().collect::<Vec<char>>()[i]).unwrap().cmp(&order.get(&a.0.chars().collect::<Vec<char>>()[i]).unwrap()));
+    }
+    pairs.sort_by(|a,b| score(b.0).cmp(&score(a.0)));
+    let score: Vec<u64> = pairs.iter().enumerate().map(|(r, t)| (r as u64 + 1)*(t.1.parse::<u64>().unwrap())).collect();
+    print!("{:?}\n", score.iter().sum::<u64>());
+}
+
+fn score (cards: &str) -> u64{
+    let count = cards.chars().collect::<counter::Counter<_>>();
+    let unique = count.len();
+    let max = count.values().max().unwrap();
+    return (5 + unique - max) as u64;
+}
